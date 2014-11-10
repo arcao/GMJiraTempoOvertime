44619886f9356f6292e8927b793d735deb4063b2
Martin Sloup <arcao@arcao.com>
2014-11-10 11:14:37 +0100

    Initial import

---
+++ jira-tempo-overtime.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Jira Tempo Overtime
+// @description Greasemonkey Overtime User script for Jira Tempo plugin
+// @author      Arcao
+// @version     1.0
+// @namespace   com.gk-software.es.msloup.jira.tempo.overtime
+// @include     /^https?://([\w\d\.-]*)jira([\w\d\.-]*)/secure/TempoUserBoard!timesheet.jspa/
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @updateURL   https://github.com/arcao/GMJiraTempoOvertime/raw/master/jira-tempo-overtime.meta.js
+// @downloadURL https://github.com/arcao/GMJiraTempoOvertime/raw/master/jira-tempo-overtime.user.js
+// @grant       GM_addStyle
+// ==/UserScript==
