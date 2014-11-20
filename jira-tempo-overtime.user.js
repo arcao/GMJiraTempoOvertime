20cf75527979123f7ac87b8b6749d1aecfac7d24
Martin Sloup <arcao@arcao.com>
2014-11-20 16:27:46 +0100

    Fixed wrong include url

--- jira-tempo-overtime.user.js
+++ jira-tempo-overtime.user.js
@@ -2,9 +2,9 @@
 // @name        Jira Tempo Overtime
 // @description Greasemonkey Overtime User script for Jira Tempo plugin
 // @author      Arcao
-// @version     1.0.4
+// @version     1.0.5
 // @namespace   com.arcao.jira.tempo.overtime
-// @include     https://jira.gk-software.com/secure/TempoUserBoard!timesheet.jspa*
+// @include     /^https?://([\w\d\.-]*)jira([\w\d\.-]*)/secure/TempoUserBoard!timesheet.jspa/
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @updateURL   https://github.com/arcao/GMJiraTempoOvertime/raw/master/jira-tempo-overtime.meta.js
 // @grant       GM_addStyle
